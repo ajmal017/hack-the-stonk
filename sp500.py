2a1c1720102f10c3d25e41ff825958193bb6685b
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-10 12:38:07 +0200

    added nasdaq data

--- sp500.py
+++ sp500.py
@@ -11,22 +11,27 @@ from data import data
 
 ticker_sp = '^GSPC'
 ticker_dow = '^DJI'
+ticker_nasdq = '^IXIC'
 
 today = date.today()
 start_date = "2000-01-01"
 
 data_sp = pdr.get_data_yahoo(ticker_sp, start=start_date, end=today)
 data_dow = pdr.get_data_yahoo(ticker_dow, start=start_date, end=today)
+data_nasdaq = pdr.get_data_yahoo(ticker_nasdq, start=start_date, end=today)
 
 prices_sp = data_sp['Adj Close']
 prices_dow = data_dow['Adj Close']
+prices_nasdaq = data_nasdaq['Adj Close']
 
 max_price_sp = max(prices_sp)
 max_price_dow = max(prices_dow)
+max_price_nasdaq = max(prices_nasdaq)
 
 plt.plot(prices_sp/max_price_sp)
 plt.plot(prices_dow/max_price_dow)
+plt.plot(prices_nasdaq/max_price_nasdaq)
 
-plt.legend(['S&P500', 'DOW JONES'])
+plt.legend(['S&P500', 'DOW JONES', 'NASDAQ'])
 
 plt.show()
