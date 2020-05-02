a0ed01de570893613e883eaf35ca0d094e1520be
JuanDelAguila <s1940569@ed.ac.uk>
2020-05-02 19:38:47 +0200

    constructed training data

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import math
 import quandl
+import numpy as np
 
 ticker_sp = '^GSPC'
 ticker_gold = 'GC=F'
@@ -95,5 +96,51 @@ def combineData():
     normalizeData(cleanData)
     return cleanData
 
-print(combineData())
+data = combineData()
+
+print(data)
+
+day_counter = 0
+
+# Size of prediction
+prediction_size = 5
+
+# Size of available data.
+data_size = len(data) - prediction_size
+
+# Size of data alocated to training
+size_training = int(0.75*data_size)
+
+# Size of data alocated to testing
+size_testing = data_size - size_training
+
+# Number of data elements in a single day
+size_day = 12 
+
+# Number of days in the past
+size_historical = 15
+
+x_train = np.zeros((size_training, size_historical, size_day))
+x_test  = np.zeros((size_testing, size_historical, size_day))
+
+for index, row in data.iterrows():
+
+    if (day_counter < len(x_train)):
+
+        for i in range (size_historical):
+            for j in range (size_day):
+                x_train[day_counter][i][j] = data.iloc[day_counter+i][j]
+            
+    #else:
+        # Append data to x_test
+    day_counter += 1
+    print("{}% completed".format(int(100 * day_counter/data_size)), end = '\r')
+
+print(x_train)
+
+
+'''
+1 - Split data into training and testing
+2 - Prepare input and output data
+'''
 
